73846d2fc0e8cb5b639a512f84c40e8b0882d963
timothytsai <timothy.tsai@noblis.org>
2017-09-07 14:52:50 -0400

    Microsite Template V1.0

---
+++ README.md
@@ -0,0 +1,120 @@
+﻿# USDOT JPO Microsite Template
+
+The microsite is a standalone website that has been templated so that small changes can be made in a couple json files to create the desired website look and functionality.  
+
+The microsite template is designed to provide users of USDOT data a new standardized entry point for them to explore USDOT DOT data and for various groups within the USDOT to create customize data home pages with the template to promote their data.  The template is not intended to be a replacement for data.transportation.gov (DTG) or the National Transportation Library (NTL) sites but instead enhance those sites by providing additional entry points to those two systems in support of the ITS JPO No Wrong Door Policy. 
+
+The website capabilities include querying desired data from a given domain, creating custom categories/buttons that search through the given domain, and displaying featured datasets for users to see on page load.
+
+The microsite template is layed out with three different sections to allow users and site manager different ways to access and promote the data.  The three sections are:
+
+- **General search box** – Text based search box to allow users to search for any term they want.  This box searches DTG and NTL for the term the user enters for the site. For example, if a user enters “weather” into the search box of the ITS JPO microsite the site will return all dataset results that have been tagged with the term “weather” somewhere in the datasets tags, title or description.
+
+- **Data Set Topics** – This section provides the user with a set of topics they can select from.  Once the user selects a topic the site pulls data based on the one or more tags corresponding  to that category topic. So, if a user selected the topic like Smart City and the input files had the tags "connected vechicle" and 'smart city' linked to that topic than the results that would be returned are data sets with those two tags
+
+- **Featured Data Set** – This section lists a series of three data sets that site manager wants to highlight.  Users clicking on one of these datasets would be taken directly to that given dataset
+
+
+## Getting Started
+
+The microsite is a template where the main files to alter is template_categories.json and template_datasets.json.
+
+### template_categories.json:
+This file contains all the category data and extra data such as domain for searching some background images and search bar placeholder text.  Below is a cut out of what the microsite looks like and the json associated with the screenshot.
+![alt text](images/screenshots/ButtonScreenshot.png "Category Layout Screenshot")
+
+Header Layout in JSON Format:
+These three fields are what controls the header.  
+1. "domain" is what will be searched for datasets.
+2. "background_image" is a chosen background image placed in "/images" will appear behind the search bar.  
+3. "Search Text" is what appears as a placeholder in the search bar.
+4. "contact_email": is the email that will be displayed in the footer of the page.
+
+```
+{
+    "domain" : "data.transportation.gov",
+    "background_image": "./images/Transportation Background.png",
+    "search_text": "",
+	"contact_email": "data.itsjpo@dot.gov",
+```
+
+Category Layout in JSON Format:
+Place the all image files for categories in the "/images/icons" for organization and reference them in the "imgIcons" field
+The tags field indicates what other search terms will also be searched along with the category name when the category is chosen/clicked.  If no tags are written in the array then the search will simply search for the category name.
+
+```
+"buttons": [
+    {
+            "CategoryName": "Connected Vehicle Message",
+            "imgIcons": "images/icons/CVPCars.png",
+			"rolloverImages": "images/icons/CVPCars_Rollover.png",
+            "altText": "car icon",
+			"tags": []
+        }, {
+            "CategoryName": "Application Message",
+            "imgIcons": "images/icons/AppMessage.png",
+			"rolloverImages": "images/icons/AppMessage_Rollover.png",
+            "altText": "car icon",
+			"tags": []
+        },
+		...
+```
+
+### template_datasets.json:
+This file contains all the dataset data.  Below is a cut out of what the microsite looks like and the json associated with the screenshot.
+![alt text](images/screenshots/DatasetsScreenshot.PNG "Datasets Layout Screenshot")
+Dataset Layout in JSON Format:
+There are five fields for each dataset:
+1. This is the name that will be displayed.  If no name is provided the title of the page that is pulled from the URL will be used.
+2. This is the description of the dataset that will be displayed.  If no description is provided the description of the page that is pulled from the URL will be used.
+3. The URL is where the data will come from.
+4. The image is the image that will be displayed.
+5. This is the alternate text for accessibility.
+
+```
+"datasets": [
+    {
+		"name": "BSM Point Map",
+		"description": "Contains all Basic Safety Messages (BSMs) collected during the Advanced Messaging Concept Development (AMCD) field testing program.",
+        "url": "https://data.transportation.gov/Automobiles/BSM-Point-Map/fgbu-uats",
+        "image": "./images/BSM.PNG",
+        "altText": "Rail road car for rail related injuries"
+    },
+    {
+		"name": "",
+		"description": "",
+        "url": "https://data.transportation.gov/Research-and-Statistics/Smart-City-Finalists-Map/gtg7-kcxb",
+        "image": "./images/SmartCity.PNG",
+        "altText": "Map of smart city finalists"
+    },
+```
+### Prerequisites
+
+The microsite is developed completely using JavaScript, CSS, and HTML and requires no installation of software to run.  The site is also designed to use the ITS JPO CSS file but could be changed to meet other USDOT groups website design needs.   JavaScript and CSS libraries are:
+
+* [JavaScript jQuery v3.2.1](http://www.dropwizard.io/1.0.2/docs/) - Used for AJAX calls
+* [JavaScript Vue](https://maven.apache.org/) - Framework used for HTML to Javascript data transfer
+* [CSS font-awesome v4.7.0](http://fontawesome.io/) - Used to style the microsite
+* [CSS bulma v0.5.0](https://bulma.io/) - Used to handle grid layouts
+
+All the libraries are included in the template and don’t need to be downloaded to run the template.
+
+### Installing
+
+1. Clone the repository
+
+2. Open and alter the template_categories.json.
+
+3. Validate the data with json format checker such as jsonformatter or jsonlint to make sure it is a valid json as it will not run without it.
+
+4. Launch it in a local webserver for testing since it is required to load the json file.
+
+
+## Versioning
+
+* **Version 1.0** -  Initial version of the template 
+
+
+## License
+
+This project is licensed under the Apache License - see the [LICENSE.txt](LICENSE.txt) file for details
---
+++ js/usdot_data_microsite_template.js
@@ -0,0 +1,388 @@
+var myVue = new Vue({
+
+    el: '#searchTerms',
+    data: {
+        background_image: '',
+        search_placeholder: '',
+
+        // functional variables - do not change
+        hideResults: 'True',
+        items: '',
+        resultsJson: [],
+        NTLJson: [],
+        responseSocrata: '',
+        // data set topics and search domain
+        // modify these values to your preference
+        url: 'https://api.us.socrata.com/api/catalog/v1?q=',
+        domain: '',
+        query: '',
+
+        // Lists for dynamic data
+        buttonlabels: [],
+        buttonTags: [],
+        altTextButtonIcons: [],
+        imgIcons: [],
+        datasets: [],
+        urls: [],
+        images: [],
+        rolloverImages: [],
+        srcs: [],
+        altTextFDS: [],
+        datasetNames: [],
+        datasetDesc: [],
+        contact_email: "",
+
+        totalDataCount: 0
+    },
+
+    // Function runs on page load
+    created: function () {
+        // Loads the initial static file of NTL Data
+        this.addNTL();
+        
+        // Loads the template data
+        this.load_json();
+
+        this.create_buttons();
+        this.loadFeaturedDatasets();
+
+        // run unit tests
+        //this.UnitTests();
+    },
+
+    methods: {
+        // Method used for sorting json data when DTG and NTL are combined
+        compare: function (a, b) {
+            var self = this;
+            const titleA = a.name.toUpperCase();
+            const titleB = b.name.toUpperCase();
+
+            let comparison = 0;
+            if (titleA > titleB) {
+                comparison = 1;
+            } else if (titleA < titleB) {
+                comparison = -1;
+            }
+            return comparison;
+        },
+
+        // function loads data.json and stores all values locally, function operates sync on load to prevent timing issue
+        load_json: function () {
+            var self = this;
+            $.ajaxSetup({
+                async: false
+            });
+            // Extracts all information out of data.json
+            $.getJSON("/microsite/template_categories.json", function (json) {
+                var i;
+                self.domain = json.domain;
+                self.background_image = json.background_image;
+                self.search_placeholder = json.search_text;
+                self.contact_email = json.contact_email;
+                for (i = 0; i < Math.min(json.maxButtonCount, json.buttons.length) ; i++) {
+                    self.buttonlabels[i] = json.buttons[i].CategoryName;
+                    self.imgIcons[i] = json.buttons[i].imgIcons;
+                    self.rolloverImages[i] = json.buttons[i].rolloverImages;
+                    var tagList = [];
+                    if (json.buttons[i].tags.length !== undefined) {
+                        for (tagIdx = 0; tagIdx < json.buttons[i].tags.length; tagIdx++) {
+                            tagList[tagIdx] = json.buttons[i].tags[tagIdx];
+                        }
+                    }
+                    self.buttonTags[i] = tagList;
+                    self.altTextButtonIcons[i] = json.buttons[i].altText;
+                }
+            });
+            // Extracts all information from datasets.json
+            $.getJSON("/microsite/template_datasets.json", function (json) {
+                var i;
+                for (i = 0; i < Math.min(json.maxDatasetCount, json.datasets.length) ; i++) {
+                    self.urls[i] = json.datasets[i].url;
+                    self.images[i] = json.datasets[i].image;
+                    self.altTextFDS[i] = json.datasets[i].altText;
+                    self.datasetNames[i] = json.datasets[i].name;
+                    self.datasetDesc[i] = json.datasets[i].description;
+                }
+            });
+            $.ajaxSetup({
+                async: true
+            });
+        },
+
+        // Finds the total count of data for header
+        initialSearch: function () {
+            var self = this;
+            $.get(self.url + '&search_context=' + self.domain, function (data) {
+                self.totalDataCount = data.results.length + self.NTLJson.length;
+            });
+        },
+
+        // gets the JSON data for a search term and sets it to the "items" variable
+        search: function (search_query) {
+            var self = this;
+            var buttonIdx;
+            var newQ = search_query;
+            for (buttonIdx = 0; buttonIdx < self.buttonlabels.length; buttonIdx++) {
+                if (search_query === self.buttonlabels[buttonIdx]) {
+                    var tagIdx;
+                    if (self.buttonTags[buttonIdx] !== undefined) {
+                        for (tagIdx = 0; tagIdx < self.buttonTags[buttonIdx].length; tagIdx++) {
+                            newQ = newQ + "+" + self.buttonTags[buttonIdx][tagIdx];
+                        }
+                    }
+                }
+            }
+            $.get(self.url + newQ + '&search_context=' + self.domain, function (data, xhr) {
+                self.resultsJson = [];
+                self.responseSocrata = xhr;
+                self.items = data;
+                self.addSocratatoJson();
+                self.addNTLtoJson(search_query);
+                self.resultsJson.sort(self.compare);
+                $('html,body').animate({ scrollTop: $("#searchresults").offset().top }, 800);
+            });
+            self.query = search_query;
+            this.hideResults = "False";
+        },
+
+        // Loads json file for NTL data, called on webpage load (will be used until NTL becomes api called)
+        addNTL: function () {
+            var itemCountNTL;
+            var self = this;
+
+            $.getJSON("/microsite/json/NTL.json", function (json) {
+                for (itemCountNTL = 0; itemCountNTL < json.datasets.length; itemCountNTL++) {
+                    var tempJson = {};
+                    tempJson["name"] = json.datasets[itemCountNTL].title;
+                    tempJson["description"] = json.datasets[itemCountNTL].Abstract;
+
+                    var tagCount;
+                    var allTags = [];
+                    var RESEARCHRESULTS = "Research Results";
+
+                    allTags[0] = RESEARCHRESULTS;
+                    for (tagCount = 0; tagCount < json.datasets[itemCountNTL].tags.length; tagCount++) {
+                        allTags[tagCount + 1] = json.datasets[itemCountNTL].tags[tagCount];
+                    }
+                    tempJson["tags"] = allTags;
+                    tempJson["tags"].sort();
+                    tempJson["link"] = json.datasets[itemCountNTL].URL;
+                    self.NTLJson.push(tempJson);
+                }
+                self.initialSearch();
+            });
+        },
+
+        // searches ntl files for match based on tag (will be used until NTL becomes API called)
+        addNTLtoJson: function (search_query) {
+            var itemCountNTL;
+            var self = this;
+            var buttonIdx = -1;
+            var tempCount;
+            // match query to button
+            for (tempCount = 0; tempCount < self.buttonlabels.length; tempCount++) {
+                if (search_query === self.buttonlabels[tempCount]) {
+                    buttonIdx = tempCount;
+                }
+            }
+            for (itemCountNTL = 0; itemCountNTL < self.NTLJson.length; itemCountNTL++) {
+                for (tagCount = 0; tagCount < self.NTLJson[itemCountNTL].tags.length; tagCount++) {
+                    // Checks if tag matches query
+                    if (self.NTLJson[itemCountNTL].tags[tagCount].toLowerCase().search(search_query.toLowerCase()) > -1) {
+                        self.resultsJson.push(self.NTLJson[itemCountNTL]);
+                        tagCount = self.NTLJson[itemCountNTL].tags.length;
+                    }
+                    else {
+                        var buttonCount;
+                        if (self.buttonTags[buttonIdx] !== undefined) {
+                            for (buttonCount = 0; buttonCount < self.buttonTags[buttonIdx].length; buttonCount++) {
+                                if (self.NTLJson[itemCountNTL].tags[tagCount].toLowerCase().search(self.buttonTags[buttonIdx][buttonCount].toLowerCase()) > -1) {
+                                    self.resultsJson.push(self.NTLJson[itemCountNTL]);
+                                    buttonCount = self.buttonTags[buttonIdx].length;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        },
+
+        // adds socrata data together with NTL for one query display
+        addSocratatoJson: function () {
+            var itemCount;
+            var self = this;
+            for (itemCount = 0; itemCount < self.items.results.length; itemCount++) {
+                var tempJson = {};
+                tempJson["name"] = self.items.results[itemCount].resource.name;
+                tempJson["description"] = self.items.results[itemCount].resource.description;
+                var tagCount;
+                var allTags = []
+                for (tagCount = 0; tagCount < self.items.results[itemCount].classification.domain_tags.length; tagCount++) {
+                    allTags[tagCount] = self.items.results[itemCount].classification.domain_tags[tagCount];
+                }
+                tempJson["tags"] = allTags;
+                tempJson["tags"].sort();
+                tempJson["link"] = self.items.results[itemCount].link;
+                self.resultsJson.push(tempJson);
+            }
+        },
+
+        // Function generates html for buttons and all attributes attached to it
+        create_buttons: function () {
+            var buttonCount = 0;
+            var self = this;
+            ////////////////
+            // Bulma wrapper section
+            var bulmaWrapper = document.createElement("div");
+            bulmaWrapper.setAttribute("class", "columns is-multiline");
+            bulmaWrapper.setAttribute("id", "bulmaDataset");
+            bulmaWrapper.setAttribute("style", "padding: 0 0 15% 0; height: 100%");
+
+            var buttonArea = document.getElementById("categoryArea");
+            buttonArea.appendChild(bulmaWrapper);
+
+            // Loops through each term/category for buttons in the provided template
+            for (i = 0; i < Math.min(10, self.buttonlabels.length) ; i++) {
+                // bulma columns
+                var bulmaCol = document.createElement("div");
+                bulmaCol.setAttribute("class", "bulmaCategories column is-one-quarter");
+
+                var imageHTML = document.createElement("IMG");
+                imageHTML.setAttribute("src", self.imgIcons[i]);
+                imageHTML.setAttribute("alt", self.altTextButtonIcons[i]);
+                imageHTML.setAttribute("style", "width: 55%; height: 55%; margin-bottom: 20%;");
+                imageHTML.setAttribute("class", "RegularThumbnail");
+                // Sets all attributes of the image
+                var imageHover = document.createElement("IMG");
+                imageHover.setAttribute("src", self.rolloverImages[i]);
+                imageHover.setAttribute("alt", self.altTextButtonIcons[i]);
+                imageHover.setAttribute("class", "HoverThumbnail");
+
+                var btn = document.createElement("button");
+                btn.setAttribute("class", "topic");
+                btn.setAttribute("id", "bterm" + i);
+                btn.setAttribute("v-on:click", "search(" + "buttonlabels[" + i + "])");
+                btn.style.verticalAlign = "middle";
+                btn.setAttribute("style", "padding-bottom: 10px;");
+                btn.appendChild(imageHTML);
+                btn.appendChild(imageHover);
+
+                var paragraphStyle = document.createElement("p");
+                paragraphStyle.innerHTML = self.buttonlabels[i].toUpperCase();
+                paragraphStyle.setAttribute("class", "categoryText");
+                btn.appendChild(paragraphStyle);
+
+                var myArea = document.getElementsByClassName("contentArea");
+                bulmaCol.appendChild(btn);
+                bulmaWrapper.appendChild(bulmaCol);
+
+                if (self.buttonlabels[i] == "") {
+                    document.getElementById("bterm" + i).parentElement.removeChild(document.getElementById("bterm" + i));
+                }
+            }
+        },
+
+        // auto-hides the search results section if True
+        // auto-hides the topic buttons and featured data sets if False
+        showFeatured: function () {
+            this.query = '';
+            $('html,body').animate({ scrollTop: $("#main").offset().top }, 800);
+            this.hideResults = "True";
+        },
+
+        // This function loads all featured data sets as html with all attributes
+        loadFeaturedDatasets: function () {
+            var self = this;
+            // bulma column list wrapper
+            var bulmaWrapper = document.createElement("div");
+            bulmaWrapper.setAttribute("class", "columns is-multiline");
+            bulmaWrapper.setAttribute("id", "bulmaDatasetFDS");
+            var datasetArea = document.getElementById("datasetArea");
+            datasetArea.appendChild(bulmaWrapper);
+            for (i = 0; i < this.urls.length; i++) {
+                var dataDiv = document.createElement("div");
+                dataDiv.setAttribute("id", "fds" + i);
+                bulmaWrapper.appendChild(dataDiv);
+            }
+            for (i = 0; i < this.urls.length; i++) {
+                var dataId = self.urls[i].substring(self.urls[i].length - 9, self.urls[i].length);
+                this.srcs[i] = "https://data.transportation.gov/w/" + dataId + "/m7rw-edbr?cur=u0WX7_BAfhk&from=root"
+                var jsonurl = "https://" + this.domain + "/views/" + dataId + ".json";
+                $.get(jsonurl, function (data) {
+                    var datasetArea = document.getElementById("bulmaDatasetFDS");
+                    //datasetArea.setAttribute("style", "text-align: center");
+                    var aRefLink = document.createElement("a");
+                    var datasetName = document.createElement("p");
+                    var imgtoBind = document.createElement("IMG");
+                    var datasetDesc = document.createElement("p");
+                    var myBreak = document.createElement("br");
+
+                    // Allows for each source to be matched by index due to async timing causing mismatch
+                    var indexofSource = 0;
+                    for (j = 0; j < self.urls.length; j++) {
+                        var currentSrc = self.urls[j].substring(self.urls[j].length - 9, self.urls[j].length);
+                        if (currentSrc == data.id) {
+                            indexofSource = j;
+                        }
+                    }
+
+                    // bulma columns
+                    var bulmaCol = document.getElementById("fds" + indexofSource);
+                    bulmaCol.setAttribute("class", "column is-one-quarter");
+                    bulmaCol.setAttribute("style", "background: #FFFFFF; margin: 5% auto; padding: 0; max-width: 50%; min-width: 300px;");
+
+                    aRefLink.setAttribute("href", self.urls[indexofSource]);
+                    aRefLink.setAttribute("target", "_blank");
+                    // DataSets name is set and stored
+                    datasetName.setAttribute("class", "featuredHeading");
+                    datasetName.innerHTML = (self.datasetNames[indexofSource] !== "") ? self.datasetNames[indexofSource] : data.name;
+                    // Sets all attributes of the image
+                    imgtoBind.setAttribute("src", self.images[indexofSource]);
+                    imgtoBind.setAttribute("alt", self.altTextFDS[indexofSource]);
+                    imgtoBind.setAttribute("class", "featuredDataThumbnail");
+                    // DataSets description is set and stored
+                    datasetDesc.setAttribute("style", "color: black; margin-left: 7%; margin-right: 7%; text-align: left");
+                    datasetDesc.innerHTML = (self.datasetDesc[indexofSource] !== "") ? self.datasetDesc[indexofSource] :
+                                            data.description == undefined ? "No Description Available" :
+                                            data.description.length > 150 ? data.description.substring(0, 150) + "..." :
+                                            data.description;
+                    // Html is appended and pieced together
+                    aRefLink.appendChild(imgtoBind);
+                    aRefLink.appendChild(datasetName);
+                    aRefLink.appendChild(datasetDesc);
+                    aRefLink.appendChild(myBreak);
+                    bulmaCol.appendChild(aRefLink);
+                    self.datasets[indexofSource] = data;
+                });
+            }
+        }
+    }
+});
+
+
+/////////
+// Jquery onclick function for a links
+var $root = $('html, body');
+$('a').click(function () {
+    $root.animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 500);
+    return false;
+});
+
+$(document).ready(function () {
+    $(window).scroll(function () {
+        //if you hard code, then use console
+        //.log to determine when you want the 
+        //nav bar to stick.  
+        //console.log($(window).scrollTop())
+        if ($(window).scrollTop() > 58) {
+            $('.navigation-bar').addClass('navbar-fixed');
+            $('.TitleText').attr('style', 'padding-top:190px');
+        }
+        if ($(window).scrollTop() < 58) {
+            $('.navigation-bar').removeClass('navbar-fixed');
+            $('.TitleText').attr('style', 'padding-top:100px');
+        }
+    });
+});
