40a78f225bee0c94260b924d1e0a908ea73da8a0
Mary VanDyke <vandyke_mary@bah.com>
2019-05-23 14:21:46 -0600

    #RDA-819 Merged new USWDS header with updated vue framework.

--- src/components/dot-header.vue
+++ src/components/dot-header.vue
@@ -1,12 +1,12 @@
 <template>
 <div>
   <a class="usa-skipnav" href="#main-content">Skip to main content</a>
-  <div class="usa-banner">
+  <div class="usa-banner site-banner">
     <div class="usa-accordion">
       <header class="usa-banner__header">
         <div class="usa-banner__inner">
           <div class="grid-col-auto">
-            <img class="usa-banner__header-flag" src="images/us_flag_small.png" alt="U.S. flag">
+            <img class="usa-banner__header-flag" src="/images/us_flag_small.png" alt="U.S. flag">
           </div>
           <div class="grid-col-fill tablet:grid-col-auto">
             <p class="usa-banner__header-text">An official website of the United States government</p>
@@ -25,7 +25,7 @@
           <div class="usa-banner__guidance tablet:grid-col-6">
             <img
               class="usa-banner__icon usa-media-block__img"
-              src="images/icon-dot-gov.svg"
+              src="/images/icon/icon-dot-gov.svg"
               alt="Dot gov">
             <div class="usa-media-block__body">
               <p>
@@ -38,7 +38,7 @@
           <div class="usa-banner__guidance tablet:grid-col-6">
             <img
               class="usa-banner__icon usa-media-block__img"
-              src="images/icon-https.svg"
+              src="/images/icon-https.svg"
               alt="Https">
             <div class="usa-media-block__body">
               <p>
@@ -53,52 +53,56 @@
       </div>
     </div>
   </div>
+
+  
   <div class="usa-overlay"></div>
   <header class="usa-header usa-header--extended" role="banner">
+    <div id="nav_top" class="usa-banner__header">
+      <div class="usa-banner__inner">
+        <a style="background: none" href="https://transportation.gov">
+          <img id="dot_logo" src="/images/dot_logo.png" style="" alt="Department of Transportation logo">
+        </a>
+      </div>
+    </div>
     <div class="usa-navbar" style="padding-top: 2rem;">
       <div class="usa-logo" id="extended-logo">
         <em class="usa-logo__text"><a href="/" title="Home" aria-label="Home">ITS DataHub</a></em>
       </div>
       <button class="usa-menu-btn">Menu</button>
     </div>
+    
     <nav role="navigation" class="usa-nav">
-      <div class="nav_top" >
-        <div class="usa-nav__inner">
-          <a style="background: none" href="https://transportation.gov">
-            <img src="images/usdot_transportation_gov-01.svg" style="height: 35px; margin-left: 25px; margin-top: 8px;" alt="Department of Transportation logo">
-          </a>
-        </div>
-      </div>
-      <div class="usa-nav__inner"><button class="usa-nav__close"><img src="images/close.svg" alt="close"></button>
+      
+      <div class="usa-nav__inner"><button class="usa-nav__close"><img src="/images/close.svg" alt="close"></button>
         <ul class="usa-nav__primary usa-accordion">
           <li class="usa-nav__primary-item">
-            <router-link class="usa-nav__link" to="/">Home</router-link>
+            <router-link class="usa-nav__link" to="/"><span>Home</span></router-link>
             <!-- <a class="usa-nav__link" href="/"><span>Home</span></a> -->
           </li>
           <li class="usa-nav__primary-item">
             <!-- <a class="usa-nav__link" href="/about"><span>About</span></a> -->
-            <router-link class="headHovers navBarLinks" to="/about">ABOUT</router-link> <div style="font-size: 15px; padding:3px 7px 3px 7px; display: inline;">|</div>
+            <router-link class="usa-nav__link" to="/about"><span>About</span></router-link>
           </li>
           <li class="usa-nav__primary-item">
             <button class="usa-accordion__button usa-nav__link" aria-expanded="false"
-              aria-controls="extended-nav-section-two"><span>Resources</span></button>
-            <ul id="extended-nav-section-two" class="usa-nav__submenu">
+              aria-controls="extended-nav-section-one"><span>Resources</span></button>
+            <ul id="extended-nav-section-one" class="usa-nav__submenu">
               <li class="usa-nav__submenu-item">
-                <a href="#">1</a>
+                <router-link class="usa-nav__link" to="#">1</router-link>
               </li>
               <li class="usa-nav__submenu-item">
-                <a href="#">2</a>
+                <router-link class="usa-nav__link" to="#">2</router-link>
               </li>
               <li class="usa-nav__submenu-item">
-                <a href="#">3</a>
+                <router-link class="usa-nav__link" to="#">3</router-link>
               </li>
             </ul>
           </li>
           <li class="usa-nav__primary-item">
-            <a class="usa-nav__link" href="/metrics"><span>Metrics</span></a>
+            <router-link class="usa-nav__link" to="/metrics"><span>Metrics</span></router-link>
           </li>
           <li class="usa-nav__primary-item">
-            <a class="usa-nav__link" href="/search"><span>Search</span></a>
+            <router-link class="usa-nav__link" to="/search"><span>Search</span></router-link>
           </li>
           <li class="usa-nav__primary-item">
             <a class="usa-nav__link" href="https://github.com/usdot-its-jpo-data-portal/microsite" target="_blank"><span>View On GitHub</span></a>
