4f30f2a3448ddc26a5c9e14a877949b31751a6fe
Mary VanDyke <vandyke_mary@bah.com>
2019-06-04 14:53:54 -0600

    #RDA-925 updated NTL rosap request URL to only return items edited starting May 24th, 2019, resulting in only the desired 20 datasets and 1 tech report being retured, drastically reducing the load time.

--- src/consts/constants.js
+++ src/consts/constants.js
@@ -3,5 +3,7 @@ export const SOCRATA_DOMAIN = 'data.transportation.gov';
 
 export const NTL_URL = 'https://rosap.ntl.bts.gov/fedora/export/view/collection/';
 export const NTL_COLLECTION = "dot:239"; //Limit results to specific collection
-export const NTL_DATELIMIT = "?from=2018-01-01T00:00:00Z"; //Limit results to before, after or between a specific date range
+export const NTL_DATELIMIT = "?from=2019-05-24T00:00:00Z"; //Limit results to before, after or between a specific date range
 export const NTL_ROWSLIMIT = "&rows=9999";
+
+//Also as promised here is an API link that ONLY grabs the 20 datasets you monitor: https://rosap.ntl.bts.gov/fedora/export/view/collection/dot:239?from=2019-05-24T00:00:00Z&until=2019-05-25T23:59:59Z
