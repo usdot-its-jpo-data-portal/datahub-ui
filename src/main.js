6d37f9c9a99f99a162e68dc855036da7152bbf9c
Gio <rodriguez_carlos@bah.com>
2019-06-12 16:20:54 -0400

    Implementation of Google Tag Manager

--- src/main.js
+++ src/main.js
@@ -2,9 +2,18 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import VueGtm from 'vue-gtm';
 import 'uswds'
 
 Vue.config.productionTip = false
+
+Vue.use(VueGtm, {
+  id: 'GTM-T6F2VPH',
+  enabled: true,
+  debug: false,
+  vueRouter: router,
+  ignoredView: []
+});
 
 new Vue({
   router,
