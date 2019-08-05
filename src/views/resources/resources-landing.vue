007bd377cb6217a510907dddc7fba61edee57ecb
Gio <rodriguez_carlos@bah.com>
2019-08-05 11:22:30 -0400

    RDA-1046 : Content for Resources landing page.

--- src/views/resources/resources-landing.vue
+++ src/views/resources/resources-landing.vue
@@ -1,7 +1,26 @@
 <template>
   <div class="usa-layout-docs__main desktop:grid-col-9 usa-prose">
-    <h1>This is the Resources Landing page</h1>
-    <h2>content should go here</h2>
+    <p>
+      Welcome to the Resources page! Here you will find resources data providers, project managers, and program managers can use to adhere to the requirements for ITS Joint Program Office’s (JPO) data management and ingestion into the Intelligent Transportation Systems (ITS) DataHub.
+    </p>
+    <ul>
+      <li>
+        <strong>Guidelines</strong>: Provides details on the ITS JPO’s data access and management requirements for data providers, project managers, and program managers.
+      </li>
+      <li>
+        <strong>Data Management and Data Storage Systems</strong>: Provides support to help adhere to the Guidelines.
+      </li>
+      <li>
+        <strong>FAQs</strong>: Find answers to the most frequently asked questions around ITS JPO Data Management.
+      </li>
+    </ul>
+    <p>
+      To provide feedback on any information provided on the Resources page, please file an issue on our
+      <a target="_blank" href="https://github.com/usdot-its-jpo-data-portal/data-access-coe-resources">GitHub<img class="in-line-dot-link-new-tab" src="/images/icons/external-tabs.svg" alt="New tab icon." title="Opens in a new tab."></a>
+      page or contact
+      <a target="_blank" href="mailto:data.itsjpo@dot.gov">data.itsjpo@dot.gov<img class="in-line-dot-link-new-tab" src="/images/icons/external-tabs.svg" alt="New tab icon." title="Opens in a new tab."></a>
+      .
+    </p>
   </div>
 </template>
 <script>
