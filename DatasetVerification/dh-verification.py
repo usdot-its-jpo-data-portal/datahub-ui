bd021ad5dbd8e844a642f390afb9b62f103a0ddc
Gio <rodriguez_carlos@bah.com>
2019-08-16 16:15:16 -0400

    RDA-1085 : Changes to support parameters through environment variables in preparation for code to run in AWS Lambda Function

--- DatasetVerification/dh-verification.py
+++ DatasetVerification/dh-verification.py
@@ -1,15 +1,18 @@
 import argparse
+from DHParams import DHParams
 from DHDatasetVerification import DHDatasetVerification
 
+def lambda_handler(event, context):
+    fromLambda = context != None
+    params = DHParams(fromLambda)
+    params.validate_params()
+    print(params)
+    if params.valid:
+        dsv = DHDatasetVerification(params)
+        dsv.execute()
+    else:
+        print(params.message)
+
 if __name__ == "__main__":
-    args = argparse.ArgumentParser(description='DataHub Dataset Verification')
-    args.add_argument('-d','--dataset', default='all', help='Dataset: all, ntl, dtg, scgc')
-    args.add_argument('-l','--list', action='store_true', default=False, help="List the result of the selected dataset")
-    args.add_argument('-j','--json', action='store_true', default=False, help="Format the list result to JSON")
-    args.add_argument('-s','--save', action='store_true', default=False, help="Save the result of the selected dataset into a text file")
-    args.add_argument('-e','--expected', default=None, help="File of expected datasets")
-    args.add_argument('config', help="Datasets configuration file")
-    params = args.parse_args()
-    dsv = DHDatasetVerification(params)
-    dsv.execute()
+    lambda_handler(None, None)
 
