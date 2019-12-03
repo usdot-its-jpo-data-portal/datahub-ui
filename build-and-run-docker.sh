4f0dec9f5e069a6e2f4989746fa02e4722613fc0
Matt S <mvs5465@gmail.com>
2019-12-03 16:21:06 -0500

    Update docker script to new proxy pass env vars

--- build-and-run-docker.sh
+++ build-and-run-docker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 docker build -t datahub-ui:latest .
-docker run --rm -d -p 8080:80 -e PROXY_PASS_URL=http://localhost:3006/api -e PROXY_PASS_APICC_URL=http://localhost:3003/apicc datahub-ui:latest
+docker run --rm -d -p 8080:80 -e PROXY_PASS_WEBAPI=http://localhost:3006/api -e PROXY_PASS_APICC=http://localhost:3003/apicc datahub-ui:latest
