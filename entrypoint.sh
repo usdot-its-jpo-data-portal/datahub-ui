95c63f813742dd92b8b9f6866cfedfb2eb14ce63
Matt S <mvs5465@gmail.com>
2019-12-03 16:27:58 -0500

    Add new proxy pass variables to entrypoint.sh script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-envsubst '${PROXY_PASS_URL},${PROXY_PASS_APICC_URL}' < /etc/nginx/nginx.template.conf > /etc/nginx/nginx.conf
+envsubst '${PROXY_PASS_WEBAPI},${PROXY_PASS_APICC}' < /etc/nginx/nginx.template.conf > /etc/nginx/nginx.conf
 nginx -g 'daemon off;'
