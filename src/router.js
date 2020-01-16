8927f6edd6c27572985a19e018a41bae8b5836ee
Gio <rodriguez_carlos@bah.com>
2020-01-16 16:54:07 -0500

    RDA-1420 : Change Router to hash mode.

--- src/router.js
+++ src/router.js
@@ -5,7 +5,7 @@ import Home from './views/Home.vue'
 Vue.use(Router)
 
 export default new Router({
-  mode: 'history',
+  mode: 'hash',
   base: process.env.BASE_URL,
   // eslint-disable-next-line
   scrollBehavior: function(to, from, savedPosition) {
