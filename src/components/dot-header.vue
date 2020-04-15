d89504ba4c558a92adc494f6a44b4183fcb1afa9
Mary VanDyke <vandyke_mary@bah.com>
2020-04-15 08:32:34 -0600

    Condensed header. Added ids to elements to test GTM in production.

--- src/components/dot-header.vue
+++ src/components/dot-header.vue
@@ -55,7 +55,7 @@
   </div>
 
   <div class="usa-overlay"></div>
-  <header class="usa-header usa-header--extended" role="banner">
+  <header class="usa-header usa-header--extended usa-header--basic" role="banner">
     <div id="nav_dot_banner" class="usa-banner__header">
       <div class="usa-banner__inner">
         <a style="background: none" href="https://transportation.gov">
@@ -63,73 +63,73 @@
         </a>
       </div>
     </div>
-    <div class="usa-navbar">
-      <div class="usa-logo" id="extended-logo">
-        <em class="usa-logo__text">
-          <a href="/" title="Home" aria-label="Home">
-            ITS DataHub
-            <div class="dh-logo__text-subtitle">Intelligent Transportation Systems</div>
-          </a>
-        </em>
-      </div>
-      <button class="usa-menu-btn" aria-controls="dh-nav">Menu</button>
-    </div>
-    
-    <nav role="navigation" class="usa-nav" id="dh-nav">
-      
-      <div class="usa-nav__inner">
-        <button class="usa-nav__close">
-          <img src="/images/close.svg" alt="close">
-        </button>
-        <ul class="usa-nav__primary usa-accordion">
-          <li class="usa-nav__primary-item">
-            <router-link aria-controls="dh-nav1" to="/" exact-active-class="router-link-exact-active" exact>Home</router-link>
-          </li>
-          <li class="usa-nav__primary-item">
-            <router-link aria-controls="dh-nav2" to="/about" active-class="router-link-active">About</router-link>
-          </li>
-          <li class="usa-nav__primary-item">
-            <router-link to="/resources" active-class="router-link-active">Resources</router-link>
-          </li>
-          <li class="usa-nav__primary-item">
-            <router-link to="/metrics" active-class="router-link-active">Metrics</router-link>
-          </li>
-          <li class="usa-nav__primary-item">
-            <router-link to="/data-access-coe" active-class="router-link-active">Community</router-link>
-          </li>
-          <li class="usa-nav__primary-item">
-            <router-link to="/search" active-class="router-link-active">Search</router-link>
-          </li>
-          <li class="usa-nav__primary-item">
-            <a href="https://github.com/usdot-its-jpo-data-portal/microsite" target="_blank" rel="noopener noreferrer">
-              <span>View On GitHub</span>
-              <img class="usa-header__primary-link-icon-social" src="/images/icons/external-link_142350.svg" alt>
+    <div class="usa-nav-container">
+      <div class="usa-navbar">
+        <div class="usa-logo" id="extended-logo">
+          <em class="usa-logo__text">
+            <a href="/" title="Home" aria-label="Home">
+              ITS DataHub
+              <div class="dh-logo__text-subtitle">Intelligent Transportation Systems</div>
             </a>
-            
-          </li>
-        </ul>
-        <div class="usa-nav__secondary" style="">
-          <div class="dh-header__secondary-links">
-            <ul class="usa-nav__secondary-links">
-              <li class="usa-nav__secondary-item">
-                <a href="https://www.its.dot.gov/" target="_blank" rel="noopener noreferrer">ABOUT ITS JPO</a>
-              </li>
-              <li class="usa-nav__secondary-item">
-                <a href="https://www.transportation.gov/mission/about-us" target="_blank" rel="noopener noreferrer">ABOUT U.S. DOT</a>
-              </li>
-              <li class="usa-nav__secondary-item">
-                <a href="https://www.transportation.gov/briefingroom" target="_blank" rel="noopener noreferrer">U.S. DOT BRIEFING ROOM</a>
-              </li>
-              <li class="usa-nav__secondary-item">
-                <a href="https://www.transportation.gov/our-activities" target="_blank" rel="noopener noreferrer">U.S. DOT ACTIVITIES</a>
-              </li>
-            </ul>
-          </div>
-          
-            
+          </em>
         </div>
+        <button class="usa-menu-btn" aria-controls="dh-nav">Menu</button>
       </div>
-    </nav>
+      <nav role="navigation" class="usa-nav" id="dh-nav">
+        <div class="usa-nav__inner">
+          <button class="usa-nav__close">
+            <img src="/images/close.svg" alt="close">
+          </button>
+          <ul class="usa-nav__primary usa-accordion">
+            <li class="usa-nav__primary-item">
+              <router-link aria-controls="dh-nav1" to="/" exact-active-class="router-link-exact-active" exact>Home</router-link>
+            </li>
+            <li class="usa-nav__primary-item">
+              <router-link aria-controls="dh-nav2" to="/about" active-class="router-link-active">About</router-link>
+            </li>
+            <li class="usa-nav__primary-item">
+              <router-link to="/resources" active-class="router-link-active">Resources</router-link>
+            </li>
+            <li class="usa-nav__primary-item">
+              <router-link to="/metrics" active-class="router-link-active">Metrics</router-link>
+            </li>
+            <li class="usa-nav__primary-item">
+              <router-link to="/data-access-coe" active-class="router-link-active">Community</router-link>
+            </li>
+            <li class="usa-nav__primary-item">
+              <router-link to="/search" active-class="router-link-active">Search</router-link>
+            </li>
+            <li class="usa-nav__primary-item">
+              <a href="https://github.com/usdot-its-jpo-data-portal/microsite" target="_blank" rel="noopener noreferrer">
+                <span>View On GitHub</span>
+                <img class="usa-header__primary-link-icon-social" src="/images/icons/external-link_142350.svg" alt>
+              </a>
+              
+            </li>
+          </ul>
+          <div class="usa-nav__secondary" style="">
+            <div class="dh-header__secondary-links">
+              <ul class="usa-nav__secondary-links">
+                <li class="usa-nav__secondary-item">
+                  <a href="https://www.its.dot.gov/" target="_blank" rel="noopener noreferrer">ABOUT ITS JPO</a>
+                </li>
+                <li class="usa-nav__secondary-item">
+                  <a href="https://www.transportation.gov/mission/about-us" target="_blank" rel="noopener noreferrer">ABOUT U.S. DOT</a>
+                </li>
+                <li class="usa-nav__secondary-item">
+                  <a href="https://www.transportation.gov/briefingroom" target="_blank" rel="noopener noreferrer">U.S. DOT BRIEFING ROOM</a>
+                </li>
+                <li class="usa-nav__secondary-item">
+                  <a href="https://www.transportation.gov/our-activities" target="_blank" rel="noopener noreferrer">U.S. DOT ACTIVITIES</a>
+                </li>
+              </ul>
+            </div>
+            
+              
+          </div>
+        </div>
+      </nav>
+    </div>
   </header>
 </div>
 </template>
