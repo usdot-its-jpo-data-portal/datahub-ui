a7e42a48c9a719ffc59cc11fe75c235b134324f2
Mary VanDyke <vandyke_mary@bah.com>
2020-10-29 14:13:44 -0400

    Updated preliminary dmp page. Added new documents under data access guidlines, templages and guides page.

--- src/views/resources/guidelines-templates-guides.vue
+++ src/views/resources/guidelines-templates-guides.vue
@@ -13,6 +13,9 @@
             <br>
             <li><a title="Click to download Microsoft Word version" href="/documents/PreliminaryDMPSufficiencyChecklist.docx">Preliminary Award DMP Sufficiency Checklist &mdash; Microsoft Word version</a></li>
             <li><a target="_blank" title="Click to view Adobe PDF version in browser" href="/documents/PreliminaryDMPSufficiencyChecklist.pdf">Preliminary Award DMP Sufficiency Checklist &mdash; Adobe PDF version <img class="in-line-dot-link-new-tab search-results" src="/images/icons/external-tabs.svg" alt="New tab icon." title="Opens in a new tab."></a></li>
+            <br>
+            <li><a title="Click to download Microsoft Word version" href="/documents/OpenLicenseGuideForData.doc">Open License Guide - Data</a></li>
+            <li><a target="_blank" title="Click to view Adobe PDF version in browser" href="/documents/OpenLicenseGuideForData.pdf">Open License Guide - Data <img class="in-line-dot-link-new-tab search-results" src="/images/icons/external-tabs.svg" alt="New tab icon." title="Opens in a new tab."></a></li>
         </ul>
     </div>
 </template>
