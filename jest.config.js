5c8cd4d57bb30b0a1a447e71712cc953d8f1d0d4
Gio <rodriguez_carlos@bah.com>
2020-11-10 12:29:07 -0500

    RDAODTD-2129: Sonar coverage report configuration.

--- jest.config.js
+++ jest.config.js
@@ -26,5 +26,16 @@ module.exports = {
   watchPlugins: [
     'jest-watch-typeahead/filename',
     'jest-watch-typeahead/testname'
+  ],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.{js,vue}',
+    '!src/main.js',
+  ],
+  coverageDirectory: "<rootDir>/test/coverage-jest",
+  coverageReporters: [
+    "lcovonly",
+    "html",
+    "text"
   ]
 }
