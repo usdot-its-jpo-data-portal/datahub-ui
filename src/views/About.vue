e573fb749cfd29e3127b77902f94a7b2c6f32423
Mary VanDyke <vandyke_mary@bah.com>
2021-02-25 14:25:37 -0500

    Updated Guidelines page and its table of contents, and about page for policy team updates.

--- src/views/About.vue
+++ src/views/About.vue
@@ -19,6 +19,20 @@
           <p>
             Many of the datasets found through ITS DataHub are published in near-real time, decreasing the time from research to insight. Check ITS DataHub often for updates including new datasets!
           </p>
+          <p>
+            No warranty or guarantee is made by the U.S. DOT as to the accuracy, reliability, relevancy, timeliness, utility, quality, availability, or completeness of the data. No active effort has been or will be made to monitor the format and any risks related to the format of the data becoming obsolete. The U.S. DOT assumes no responsibility for errors or omissions found in data discovered through ITS DataHub. The U.S. DOT reserves the right to discontinue availability of content on ITS DataHub at any time and for any reason.
+          </p>
+          <p>
+            U.S. DOT disclaims any responsibility or legal liability whatsoever, including payment of any damages of any kind, such as, lost profits, lost savings, or any other incidental or consequential damages. U.S. DOT makes such disclaimer of any and all claims based on its provision of the data, your use or reliance on the data, or on any requests for technical assistance through <a href="mailto:data.itsjpo@dot.gov">data.itsjpo@dot.gov</a> or the U.S. DOT. The user assumes the entire risk associated with the use or misuse of the data.
+          </p>
+          <p>
+            Users should also review any relevant disclaimers found within storage systems used by ITS DataHub and/or datasets discoverable through ITS DataHub.
+          </p>
+
+          <h2 id="section-heading-h2">Disclaimer</h2>
+          <p>
+            The data discoverable through ITS DataHub is offered as a public service by the U.S. Department of Transportation (U.S. DOT) and is expressly provided “AS IS” and “AS AVAILABLE”.
+          </p>
 
           <h2 id="section-heading-h2">Contact Information</h2>
           <p>
