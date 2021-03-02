0092d32d35bdd3abeb4c5a34c5ba275cf18ab395
Mary VanDyke <vandyke_mary@bah.com>
2021-03-02 11:14:24 -0500

    Updated unit tests.

--- tests/unit/views/about.spec.js
+++ tests/unit/views/about.spec.js
@@ -13,7 +13,7 @@ describe('DOT Microsite About', () => {
     router = new VueRouter({routes});
   });
 
-  it('constains main title', () => {
+  it('contains main title', () => {
     const wrapper = shallowMount(about, { attachTo: document.body, localVue, router });
     let v = wrapper.find('h1');
     expect(v.text()).toMatch('About');
@@ -21,13 +21,14 @@ describe('DOT Microsite About', () => {
   it('has the expected number of H2 elements', () => {
     const wrapper = shallowMount(about, { attachTo: document.body, localVue, router });
     let h2s = wrapper.findAll('h2');
-    expect(h2s.length).toEqual(2);
+    expect(h2s.length).toEqual(3);
   });
   it('has the expected H2 elements', () => {
     const wrapper = shallowMount(about, { attachTo: document.body, localVue, router });
     let h2s = wrapper.findAll('h2');
     let expected = [
       'Available Data',
+      'Disclaimer',
       'Contact Information'
     ];
     for(let i=0; i<h2s.length; i++){
